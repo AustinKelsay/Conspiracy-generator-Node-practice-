eaf1723d3eaabfd084996d43edfd1824f8c5784b
AustinKelsay <austinkelsay@yahoo.com>
2020-04-19 17:23:40 -0500

    Red pill

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Conspiracy-generator-Node-practice-
+
+
+SETUP:
+
+- Navigate to root directory
+
+- Run "node conspiracy-generator.js"
+
+- Beware of government surveilance
---
+++ conspiracy-generator.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+
+//this is going to be our directory
+const dir = "conspiracies"
+
+//this is going to make a directory on our local file system
+fs.mkdirSync(dir)
+
+for (let i = 0; i <= 100; i++) {
+    //creates new file
+    fs.writeFileSync(`${dir}/${i}.txt,` `Epstien didn't kill himself.`)
+}
